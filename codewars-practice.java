1712fe07930aeedddffa1a01a76568858d557e85
augustkellerwrites <guskeller@gmail.com>
2026-04-18 10:17:08 -0500

    exercise 1 prepped

---
+++ codewars-practice.java
@@ -0,0 +1,15 @@
+//Exercise 1
+//Complete the solution so that it reverses the string passed into it.
+//Example:
+//'world'  =>  'dlrow'
+//'word'   =>  'drow'
+public class Kata {
+
+  public static String solution(String str) {
+    if (str.length < 2){
+      return str
+    } else {
+      return str[str.length - 1] + solution(str - 1)
+    }
+  }
+}
